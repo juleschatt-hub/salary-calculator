e801c2923c61aad8beeff3b88949e2f1ba92d0fd
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-05-03 01:31:02 -0500

    input fields now clear

--- script.js
+++ script.js
@@ -12,36 +12,38 @@ function addEmployee(event) {
     console.log('form submits');
     //stops page refresh on submit
     event.preventDefault();
+//creating a querySelector to target each input field by ID
+    let firstNameVal = document.querySelector('#firstName');
+    let lastNameVal = document.querySelector('#lastName');
+    let employeeIDVal = document.querySelector('#employeeID');
+    let jobTitleVal = document.querySelector('#jobTitle');
+    let annualSalaryVal = document.querySelector('#annualSalary');
 
-    let firstNameVal = document.querySelector('#firstName').value;
-    let lastNameVal = document.querySelector('#lastName').value;
-    let employeeIDVal = document.querySelector('#employeeID').value;
-    let jobTitleVal = document.querySelector('#jobTitle').value;
-    let annualSalaryVal = document.querySelector('#annualSalary').value;
-
-    //Output to DOM must be in the form of a table on the index file
+//Output to DOM must be in the form of a table on the index file
     let employeeTable = document.querySelector('#employeeData');
     employeeTable.innerHTML += `
     
     <tr>
-        <td>${firstNameVal}</td>
-        <td>${lastNameVal}</td>
-        <td>${employeeIDVal}</td>
-        <td>${jobTitleVal}</td>
-        <td>${annualSalaryVal}</td>
+        <td>${firstNameVal.value}</td>
+        <td>${lastNameVal.value}</td>
+        <td>${employeeIDVal.value}</td>
+        <td>${jobTitleVal.value}</td>
+        <td>${annualSalaryVal.value}</td>
         <td><button onClick="removeEmployee(event)">Delete</button></td>
     </tr>
 
     `
    
+    //creating an employee object to target individual employee salaries
     let employee = {
-        first: `${firstNameVal}`,
-        last: `${lastNameVal}`,
-        id: `${employeeIDVal}`,
-        title: `${jobTitleVal}`,
-        salary: `${annualSalaryVal}`
+        first: `${firstNameVal.value}`,
+        last: `${lastNameVal.value}`,
+        id: `${employeeIDVal.value}`,
+        title: `${jobTitleVal.value}`,
+        salary: `${annualSalaryVal.value}`
     }
     
+    //pushes a newly created employee to an array called employees
     employees.push(employee);
     totalMonthly = Number(employee.salary) / 12 + totalMonthly;
     console.log(employees);
@@ -53,12 +55,13 @@ function addEmployee(event) {
     
     `
 
-    //attempt to sanitize inputs NOT WORKING
-    firstNameVal = '';
-    lastNameVal = '';
-    employeeIDVal = 0;
-    jobTitleVal = '';
-    annualSalaryVal = 0;
+    //attempt to clear input fields NOT WORKING!!!!!!!!!!
+    firstNameVal.value = '';
+    lastNameVal.value = '';
+    employeeIDVal.value = '';
+    jobTitleVal.value = '';
+    annualSalaryVal.value = '';
+    console.log('firstnameval: ', firstNameVal);
 }
 
 
