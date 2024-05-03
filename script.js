1ffc00044d104da65826a36209400dc28a4adc69
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-05-02 19:15:31 -0500

    Added form and inputs to index file

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/**************************************************************************                      
+                       SUBMIT BUTTON REQUIREMENTS   
+***************************************************************************/
+//submit button onclick function that adds a new employee to the table
+//Adds employees salary to the total payroll cost for the company
+//sanitize inputs after emplyee is added.
+
+
+//Output to DOM must be in the form of a table on the index file
+//The total employee cost must be in the footer element.
+
+
+/**************************************************************************                      
+                       DELETE BUTTON REQUIREMENTS   
+***************************************************************************/
+//delete button in each row that removes employee from table
+//(STRETCH: removed employees salary is removed from total)
